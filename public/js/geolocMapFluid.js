63c880aa5c5e7954343b98c1f84210fcc98f4bd6
Ninodev14 <nino.lefort@mmibordeaux.com>
2025-02-14 17:02:57 +0100

    add mouve map

--- public/js/geolocMapFluid.js
+++ public/js/geolocMapFluid.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleMapSizeBtn = document.getElementById("toggleMapSizeBtn");
   const mapElement = document.getElementById("map");
+  const leftColumn = document.querySelector(".col-md-3");
 
   let isFullScreen = false;
 
   toggleMapSizeBtn.addEventListener("click", () => {
     if (isFullScreen) {
-      // Si la carte est en plein écran, réinitialise la taille
       mapElement.classList.remove("full-screen-map");
+      leftColumn.classList.remove("full-screen-left-column");
       toggleMapSizeBtn.innerHTML =
         '<i class="bi bi-arrow-expand"></i> Agrandir la carte';
       isFullScreen = false;
     } else {
       // Si la carte est dans sa taille initiale, met-la en plein écran
       mapElement.classList.add("full-screen-map");
+      leftColumn.classList.add("full-screen-left-column");
       toggleMapSizeBtn.innerHTML =
         '<i class="bi bi-arrow-collapse"></i> Réduire la carte';
       isFullScreen = true;
