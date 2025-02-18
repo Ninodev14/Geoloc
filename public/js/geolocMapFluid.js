6d3034d4bedd3d913efe5182fb289ff87a957ea7
Ninodev14 <nino.lefort@mmibordeaux.com>
2025-02-18 14:24:15 +0100

    add arrow

--- public/js/geolocMapFluid.js
+++ public/js/geolocMapFluid.js
@@ -9,15 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
     if (isFullScreen) {
       mapElement.classList.remove("full-screen-map");
       leftColumn.classList.remove("full-screen-left-column");
-      toggleMapSizeBtn.innerHTML =
-        '<i class="bi bi-arrow-expand"></i> Agrandir la carte';
+      toggleMapSizeBtn.innerHTML = '<i class="bi bi-arrow-expand"></i> <';
       isFullScreen = false;
     } else {
-      // Si la carte est dans sa taille initiale, met-la en plein écran
       mapElement.classList.add("full-screen-map");
       leftColumn.classList.add("full-screen-left-column");
-      toggleMapSizeBtn.innerHTML =
-        '<i class="bi bi-arrow-collapse"></i> Réduire la carte';
+      toggleMapSizeBtn.innerHTML = '<i class="bi bi-arrow-collapse"></i> >';
       isFullScreen = true;
     }
   });
